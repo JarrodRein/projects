2842f2e61d0c59701410ead7f5fb0a15385b5df0
Jarrod Rein <jrein@uco.edu>
2023-01-08 10:44:15 -0600

    solved

--- HackRank/StringStream.cpp
+++ HackRank/StringStream.cpp
@@ -5,18 +5,35 @@ using namespace std;
 
 vector<int> parseInts(string str) {
 	// Complete this function
+    stringstream ss(str);
+char ch;
+int a, b, c;
+//ss >> a >> ch >> b >> ch >> c;  // a = 23, b = 4, c = 56   
+  //  cout << a << b << c << endl;  
     vector <int> vec;
-    
-    int num =0;
+    int num1 =0;
     for(int i =0; i< str.length(); i++){
-        
        // int num = (int)str[i];
-       if(str[i]==',' || str[i] ==' '){
-           i++;
-       }
-        
-        vec.push_back(num);
+       
+    ss >> a >> ch;
+    vec.push_back(a);
+    
+    
+    
+    
+    int num = vec.size();
+ //  cout << num << endl;
+   if( a == num1){
+       vec.pop_back();
+       return vec;
+   }
+   
+    num1 = a;
+       // vec.push_back(a);
     }
+    
+    
+    
     return vec;
 }
 
