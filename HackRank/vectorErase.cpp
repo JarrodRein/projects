0993138d399d3e1adbd6945acfda6e27660e5600
Jarrod Rein <jarrod1rein@gmail.com>
2023-01-12 15:21:55 -0600

    almost there

---
+++ HackRank/vectorErase.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    
+    int long num1, num2, num3, num4, num5;
+    cin >> num1;
+    vector <int> vec;
+    for(int i=0; i< num1; i++){
+        cin >> num2;
+        vec.push_back(num2);
+        
+        
+    }  
+    
+    cin >> num3;
+    
+    vec.erase(vec.begin()+num3);
+    
+    cin >> num4 >> num5;
+    
+    num4 -=1;
+    num5 -=1;
+    vec.erase(vec.begin()+num4, vec.begin()+num5);
+    
+    int long num7 = vec.size();
+    cout << num7 << endl;
+    
+    for(int j=0; j<num7; j++){
+            cout << vec[j] << " ";
+
+    }
+    return 0;
+}
