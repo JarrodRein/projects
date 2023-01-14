2c0dff121126c3a5f142e783ee2a293c21021253
Jarrod Rein <jarrod1rein@gmail.com>
2023-01-13 22:28:50 -0600

    attemping

---
+++ LeetCode/intersectionOfTwoArraysII.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        vector <int> vec;
+       int count =0;
+        for(int i=0; i<nums1.size(); i++){
+            for(int j=0; j<nums2.size(); j++){
+                if(nums1[i]==nums2[j] && count ==0){
+                    count++;
+                    vec.push_back(nums1[i]);
+                }
+                count =0;
+            }
+        }
+        return vec;
+    }
+};
