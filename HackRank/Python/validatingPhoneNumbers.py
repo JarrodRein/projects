afee7004f35f0199136b6c48f4e2197a01389a98
Jarrod Rein <jrein@uco.edu>
2023-01-23 13:21:21 -0600

    solved

--- HackRank/Python/validatingPhoneNumbers.py
+++ HackRank/Python/validatingPhoneNumbers.py
@@ -1,4 +1,16 @@
+import re
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 if __name__ == '__main__':
     n = int(input())
-    
+    i=1
+    while i <= n:
+        m= str(input())
+       # j = int(m)
+        match = re.search(r"^[7-9][0-9]{9}$", m)
+        if match:
+            print("YES")
+        else:
+            print("NO")
+       # print(m)
+       # print(i, end='')
+        i +=1
