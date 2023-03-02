72a2f3370699d496301b08305d4c830a80a2a166
Jarrod Rein <jarrod1rein@gmail.com>
2023-03-02 11:31:51 -0600

    solved

--- HackRank/C++/maps-stl.cpp
+++ HackRank/C++/maps-stl.cpp
@@ -18,44 +18,32 @@ int main() {
     
     for(int i=0; i<num1; i++){
         cin >> num2; 
-        if(num2 == 3){
-        //cout << num2 <<" type"<<endl;
-
+        
+        if(num2 == 2 || num2 ==3){
              cin >> word;
-    int num5 = map[word];
-    cout << num5 << endl;
+    map.insert(make_pair(word, 0));
 
-        } else if(num2 == 2){
-     //   cout << num2 <<" type"<<endl;
-
-             cin >> word;
-             int num4 =0;
-    map[word] =num4;
-
-        }
-        else{
-    //cout << num2 <<" type"<<endl;
-
+        } else{
             cin >> word >> num3;
-           // cout<< "input " << word<<" " << num3 << endl;
-    //map.insert(make_pair(word, num3));
-    map[word] +=num3;
+            cout << word << num3 << endl;
+    map.insert(make_pair(word, num3));
+
         }
         
         
       //  cout << num2 <<" "<<  word  <<" "<< num3 << endl;
         
-        // num2=0;
-        // num3 =0;
-        // word ="";
+        num2=0;
+        num3 =0;
+        word ="";
     }
     
   //  map.insert(make_pair("hello", 9));
     
-//     for(auto it = map.cbegin(); it != map.cend(); ++it)
-// {
-//     std::cout << it->first << " " << it->second  << "\n";
-// }
+    for(auto it = map.cbegin(); it != map.cend(); ++it)
+{
+    std::cout << it->first << " " << it->second  << "\n";
+}
 
     
      
