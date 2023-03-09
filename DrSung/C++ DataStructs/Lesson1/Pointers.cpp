e838eabc44a591e9373c9d1d55117624d9870d43
Jarrod Rein <jrein@uco.edu>
2023-03-08 18:28:21 -0600

    two linked extern files

--- DrSung/C++ DataStructs/Lesson1/Pointers.cpp
+++ DrSung/C++ DataStructs/Lesson1/Pointers.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 extern int add(int, int);
+extern int mul(int, int);
 
 int main()
 {
     int n = add(10, 20);
+    cout << n << endl;
+    n = mul(10, 20);
     cout << n << endl;
 }
 
