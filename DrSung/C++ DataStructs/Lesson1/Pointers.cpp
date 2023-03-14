6701a776f3f746892c266ebd9f3aeb2bc7b05c43
Jarrod Rein <jrein@uco.edu>
2023-03-13 21:29:54 -0500

    pointers concept

--- DrSung/C++ DataStructs/Lesson1/Pointers.cpp
+++ DrSung/C++ DataStructs/Lesson1/Pointers.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 extern int add(int, int);
 extern int mul(int, int);
+extern void pointer_concept();
 
 int main()
 {
-    int n = add(10, 20);
-    cout << n << endl;
-    n = mul(10, 20);
-    cout << n << endl;
+//     int n = add(10, 20);
+//     cout << n << endl;
+//     n = mul(10, 20);
+//     cout << n << endl;
+
+pointer_concept();
 }
 
--- DrSung/C++ DataStructs/Lesson1/prog1.cpp
+++ DrSung/C++ DataStructs/Lesson1/prog1.cpp
@@ -3,3 +3,7 @@ int add(int a, int b)
     int r = a + b;
     return r;
 }
+
+void pointer_concept(){
+    
+}
