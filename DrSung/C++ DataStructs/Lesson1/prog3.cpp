16d79a866cfc747e8ad6a7d6ad736a619e0a9c32
Jarrod Rein <jrein@uco.edu>
2023-03-15 20:47:04 -0500

    shifting through pointers

--- DrSung/C++ DataStructs/Lesson1/prog3.cpp
+++ DrSung/C++ DataStructs/Lesson1/prog3.cpp
@@ -10,19 +10,40 @@ using namespace std;
 void pointer_arithmetic(){
     cout << "pointer arithmetic" << endl;
 
-    int n = 15;
+    // int n = 15;
 
-    int* p =&n;
+    // int* p =&n;
 
-    assert(n == *p);
+    // assert(n == *p);
 
-    cout << *p << " " << n << endl;
+    // cout << *p << " " << n << endl;
 
-    n = 20;
+    // n = 20;
 
-    assert(n == *p);
-    *p = 30;
-    assert(n == *p);
+    // assert(n == *p);
+    // *p = 30;
+    // assert(n == *p);
     
+    int a[5]= {0, 1, 2, 3, 4};
+
+    //the name of the array == the address of the first element
+    assert(a ==&a[0]);
     
+    int* p1 = a;
+    int* p2 = &a[0];
+    assert(p1 ==p2);
+
+    p1 += 1;
+    assert(p1 ==&a[1]);
+
+      p1++;
+    assert(p1 ==&a[2]);
+
+    for(int i=0; i<5; i++){
+        assert(*p2 ==a[i]);
+        ++p2;
+        
+    }
+    
+
 }
