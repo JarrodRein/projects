669d33ea9c20cdcbcbcbdb6ca457a19b6bb3cb98
Jarrod Rein <jrein@uco.edu>
2023-05-10 13:55:21 -0500

    Q4

---
+++ VisualStudio/Q1 Program 4/Q1 Program 4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter speed limit and speed");
+Console.WriteLine("Enter speed limit and press enter");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter speed and press enter");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+string result = (userNumber > userNumber2) ? "OK" : "Not OK";
+int points = 0;
+
+for(var i =userNumber2; i>userNumber; i -= 5)
+{
+    points++;
+}
+
+if(points >= 12)
+{
+    Console.WriteLine("License Suspended");
+}
+
+Console.WriteLine(points);
