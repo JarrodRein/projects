e0203b5ce784d32a77c5dcc2ecc193f287655a84
Jarrod Rein <jrein@uco.edu>
2023-05-10 13:33:46 -0500

    Q3

---
+++ VisualStudio/Q1 Program 3/Q1 Program 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter height and width");
+Console.WriteLine("Enter height and press enter");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter width and press enter");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+string result = (userNumber > userNumber2) ? "Portrait" :  "LandScape";
+Console.WriteLine(result);
