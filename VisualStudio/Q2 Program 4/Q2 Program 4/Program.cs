b195c0983fe0f9e36bf83c3e5709353059bb7361
Jarrod Rein <jarrodrein@gmail.com>
2023-05-11 16:53:12 -0500

    Guess random number

--- VisualStudio/Q2 Program 4/Q2 Program 4/Program.cs
+++ VisualStudio/Q2 Program 4/Q2 Program 4/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int answer = 0;
+Random rnd = new Random();
+int collect = rnd.Next(1, 10);
+//Console.WriteLine(collect);
+//List<int> ints = new List<int>();
+int count = 0;
+
+
+
+while (true)
+{
+    Console.WriteLine("Type number between 1-10 to guess random number");
+
+    answer = Convert.ToInt32(Console.ReadLine());
+    if (answer == collect)
+    {
+        Console.WriteLine("Correct Guess!");
+
+        break;
+    }
+    else if (count == 3){
+        Console.WriteLine("Game over");
+        break;
+
+    }
+    else
+    {
+        count++;
+        Console.WriteLine("Guess Again");
+
+    }
+
+
+   
+
+
+
+
+
+}
+//var resultsOfSum = 0;
+//foreach (var inter in ints)
+//{
+//    resultsOfSum += inter;
+//}
+
+//string result = string.Join(", ", ints.ToArray());
+//Console.WriteLine(result);
+//Console.WriteLine(resultsOfSum);
