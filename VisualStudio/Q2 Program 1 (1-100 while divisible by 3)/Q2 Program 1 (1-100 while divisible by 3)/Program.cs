8aeec20b7dd486316ec6711366b5758aa6f2758b
Jarrod Rein <jrein@uco.edu>
2023-05-11 07:18:31 -0500

    all the number divisible by 3 under 100

---
+++ VisualStudio/Q2 Program 1 (1-100 while divisible by 3)/Q2 Program 1 (1-100 while divisible by 3)/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<int> ints = new List<int>();
+
+    for(var i=0; i<=100; i++)
+{
+    if(i % 3 == 0)
+    {
+        ints.Add(i);
+    }
+   // ints.Add(i);
+}
+int count = ints.Count();
+string result = string.Join(", ", ints.ToArray());
+Console.WriteLine(result);
+Console.WriteLine(count);
+
