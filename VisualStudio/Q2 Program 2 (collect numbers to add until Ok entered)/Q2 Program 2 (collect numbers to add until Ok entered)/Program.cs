ce2c7400b01ba7131ebe808b8921391f2f865016
Jarrod Rein <jarrodrein@gmail.com>
2023-05-11 09:08:21 -0500

    test

---
+++ VisualStudio/Q2 Program 2 (collect numbers to add until Ok entered)/Q2 Program 2 (collect numbers to add until Ok entered)/Program.cs
@@ -0,0 +1,32 @@
+﻿string answer = "okkk";
+int collect = 0;
+List<int> ints = new List<int>();
+while(true)
+{
+    Console.WriteLine("Type Ok to add all number together");
+
+    answer = Console.ReadLine();
+    if( answer == "Ok" || answer == " " || answer == "ok")
+    {
+        break;
+    } 
+
+
+    collect=  Convert.ToInt32(answer);
+  //  Console.WriteLine(collect);
+    ints.Add(collect);
+
+
+
+
+
+}
+var resultsOfSum =0;
+foreach(var inter in ints)
+{
+    resultsOfSum += inter;
+}
+
+string result = string.Join(", ", ints.ToArray());
+Console.WriteLine(result);
+Console.WriteLine(resultsOfSum);
