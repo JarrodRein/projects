6e48e1ea497c47a85f7a0cb96f53ed2308bdf288
Jarrod Rein <jarrodrein@gmail.com>
2023-05-11 20:03:27 -0500

    done

---
+++ VisualStudio/Q2 Program 5 (enter numbers and return largest number)/Q2 Program 5 (enter numbers and return largest number)/Program.cs
@@ -0,0 +1,35 @@
+﻿string answer = "okkk";
+int collect = 0;
+List<int> ints = new List<int>();
+while (true)
+{
+    Console.WriteLine("Type Ok to find the largest of all numbers");
+
+    answer = Console.ReadLine();
+    if (answer == "Ok" || answer == " " || answer == "ok")
+    {
+        break;
+    }
+
+
+    collect = Convert.ToInt32(answer);
+    //  Console.WriteLine(collect);
+    ints.Add(collect);
+
+
+
+
+
+}
+var resultsOfSum = 0;
+foreach (var inter in ints)
+{
+    if(inter > resultsOfSum)
+    {
+        resultsOfSum = inter;
+    }
+}
+
+string result = string.Join(", ", ints.ToArray());
+Console.WriteLine(result);
+Console.WriteLine(resultsOfSum);
