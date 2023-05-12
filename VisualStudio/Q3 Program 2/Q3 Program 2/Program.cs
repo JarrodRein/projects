3c6a580f38c38192e86b3562f7369d984e542b8f
Jarrod Rein <jarrodrein@gmail.com>
2023-05-12 11:11:19 -0500

    write name in reverse

--- VisualStudio/Q3 Program 2/Q3 Program 2/Program.cs
+++ VisualStudio/Q3 Program 2/Q3 Program 2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string answer = "okkk";
+string collect = " ";
+List<string> ints = new List<string>();
+
+var words = new string[2];
+
+    Console.WriteLine("Type your name");
+
+    collect = Console.ReadLine();
+char[] cArr = collect.ToCharArray();
+Array.Reverse(cArr);
+Console.WriteLine(cArr);
+    
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+string results = string.Join(", ", ints.ToArray());
+
+Console.WriteLine(collect);
+
