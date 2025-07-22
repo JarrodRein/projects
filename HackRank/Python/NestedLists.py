d33df66349e82dc29501ba65188a1b63e233efbf
jarrodrein <jarrod1rein@gmail.com>
2025-07-22 09:26:54 -0500

    printing names

--- HackRank/Python/NestedLists.py
+++ HackRank/Python/NestedLists.py
@@ -1,12 +1,17 @@
 if __name__ == '__main__':
+    students = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
         students.append([name, score])
+       # print(name)
 
-    scores = sorted(set([s[1] for s in students]))
+    scores = [s[1] for s in students]
+    sorted(scores)
     second_lowest = scores[1]
+    print(score[1])
 
-    result = sorted([name for name, score in students if score == second_lowest])
+    result = [name for name, score in students if score == second_lowest]
+    sorted(result)
     for name in result:
         print(name)
