aa3b35d9f8c087c795e2922ddf38f2641ad5a249
jarrodrein <jarrod1rein@gmail.com>
2025-07-23 18:07:13 -0500

    better code

--- LeetCode/Python/AddTwoNumbers.py
+++ LeetCode/Python/AddTwoNumbers.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -5,8 +7,8 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        dummy = ListNode(0)
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
         current = dummy
         carry = 0
 
@@ -19,7 +21,9 @@ class Solution:
             current.next = ListNode(total % 10)
             current = current.next
 
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
         return dummy.next
