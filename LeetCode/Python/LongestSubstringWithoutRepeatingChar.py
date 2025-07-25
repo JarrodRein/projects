9d8215848e9d1b98447aa16f2834a6f2251747fd
jarrodrein <jarrod1rein@gmail.com>
2025-07-25 09:46:12 -0500

    loops set

--- LeetCode/Python/LongestSubstringWithoutRepeatingChar.py
+++ LeetCode/Python/LongestSubstringWithoutRepeatingChar.py
@@ -2,4 +2,8 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         char_set = set()
         left = 0
-       
+        max_length = 0
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+               
