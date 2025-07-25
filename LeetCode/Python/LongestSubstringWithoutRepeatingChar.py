5edaee926b85446cde4e13dac3ea77999180ee58
jarrodrein <jarrod1rein@gmail.com>
2025-07-25 09:46:22 -0500

    final commit

--- LeetCode/Python/LongestSubstringWithoutRepeatingChar.py
+++ LeetCode/Python/LongestSubstringWithoutRepeatingChar.py
@@ -6,4 +6,9 @@ class Solution:
 
         for right in range(len(s)):
             while s[right] in char_set:
-               
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
