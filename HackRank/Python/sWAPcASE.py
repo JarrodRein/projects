297f6aa7ca8484a53b2c2354c7883438f86f069c
jarrodrein <jarrod1rein@gmail.com>
2025-07-28 10:05:12 -0500

    final commit

--- HackRank/Python/sWAPcASE.py
+++ HackRank/Python/sWAPcASE.py
@@ -1,5 +1,5 @@
 def swap_case(s):
-    return s.swapcase()
+    return s.swapcase() # This method returns a new string with all uppercase letters converted to lowercase and vice versa.
 
 if __name__ == '__main__':
     s = input()
