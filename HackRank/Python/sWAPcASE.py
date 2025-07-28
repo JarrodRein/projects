b6c8f5d6a988261c25c1f07942670fb6309c1a69
jarrodrein <jarrod1rein@gmail.com>
2025-07-28 10:09:23 -0500

    final commit

--- HackRank/Python/sWAPcASE.py
+++ HackRank/Python/sWAPcASE.py
@@ -5,3 +5,18 @@ if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
+
+
+#manually scripted
+# This function iterates through each character in the string `s`, checks if it is lowercase or uppercase, and swaps the case accordingly.
+    # The result is then printed.
+def swap_case(s):
+    result = ''
+    for char in s:
+        if char.islower():
+            result += char.upper()
+        elif char.isupper():
+            result += char.lower()
+        else:
+            result += char
+    return result
