da7cf602a73c1d1762d4b44ced3ccf8c508b53f5
jarrodrein <jarrod1rein@gmail.com>
2025-07-28 10:11:41 -0500

    test

--- HackRank/Python/StringSplitAndJoin.py
+++ HackRank/Python/StringSplitAndJoin.py
@@ -1,7 +1,9 @@
 
 
 def split_and_join(line):
-    # write your code here
+    line = line.split(" ")  # Split the string into a list of words
+    return "-".join(line)  # Join the list into a string with hyphens
+
 
 if __name__ == '__main__':
     line = input()
