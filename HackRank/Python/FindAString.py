b6d50856cf9f72525eb522b703bc024d0821ff32
jarrodrein <jarrod1rein@gmail.com>
2025-07-30 17:08:10 -0500

    final commit

--- HackRank/Python/FindAString.py
+++ HackRank/Python/FindAString.py
@@ -1,8 +1,10 @@
 def count_substring(string, sub_string):
     count =0
-    m = len(string)
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i + len(sub_string)] == sub_string:
+            count += 1
+    return count
     
-    return
 
 if __name__ == '__main__':
     string = input().strip()
