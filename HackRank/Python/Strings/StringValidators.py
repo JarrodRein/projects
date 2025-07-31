b476952297f3ecf45de4e1b16c94e5ba437bb1ee
jarrodrein <jarrod1rein@gmail.com>
2025-07-31 08:18:26 -0500

    for loops for each letter

--- HackRank/Python/Strings/StringValidators.py
+++ HackRank/Python/Strings/StringValidators.py
@@ -1,7 +1,39 @@
 if __name__ == '__main__':
     s = input()
-    print(s.isalnum())
-    print(s.isalpha())
-    print(s.isdigit())
-    print(s.islower())
-    print(s.isupper())
+    for i in range(0, len(s)):
+        if s[i].isalnum():
+            print("True")
+        else:
+            print("False")
+        break
+    for i in range(0, len(s)):
+            if s[i].isalpha():
+                print("True")
+            else:
+                print("False")
+            break
+    #print(s.isalnum())
+   # print(s.isalpha())
+    for i in range(0, len(s)):
+                if s[i].isdigit():
+                    print("False")
+                else:
+                    print("True")
+                break
+   
+   # print(s.isdigit())
+   # print(s.islower())
+    for i in range(0, len(s)):
+            if s[i].islower():
+                print("True")
+            else:
+                print("False")
+            break
+    #print(s.isupper())
+    
+    for i in range(0, len(s)):
+            if s[i].isupper():
+                print("False")
+            else:
+                print("True")
+            break
