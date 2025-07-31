8f87d40d60a46508da6eba384bf7e5dc861a3349
jarrodrein <jarrod1rein@gmail.com>
2025-07-31 08:30:44 -0500

    final commit

--- HackRank/Python/Strings/StringValidators.py
+++ HackRank/Python/Strings/StringValidators.py
@@ -1,39 +1,42 @@
 if __name__ == '__main__':
     s = input()
-    for i in range(0, len(s)):
-        if s[i].isalnum():
-            print("True")
-        else:
-            print("False")
-        break
-    for i in range(0, len(s)):
-            if s[i].isalpha():
-                print("True")
-            else:
-                print("False")
-            break
-    #print(s.isalnum())
-   # print(s.isalpha())
-    for i in range(0, len(s)):
-                if s[i].isdigit():
-                    print("False")
-                else:
-                    print("True")
-                break
-   
-   # print(s.isdigit())
-   # print(s.islower())
-    for i in range(0, len(s)):
-            if s[i].islower():
-                print("True")
-            else:
-                print("False")
-            break
-    #print(s.isupper())
     
-    for i in range(0, len(s)):
-            if s[i].isupper():
-                print("False")
-            else:
-                print("True")
+    # Check if any character is alphanumeric
+    for c in s:
+        if c.isalnum():
+            print("True")
             break
+    else:
+        print("False")
+
+    # Check if any character is alphabetic
+    for c in s:
+        if c.isalpha():
+            print("True")
+            break
+    else:
+        print("False")
+
+    # Check if any character is a digit
+    for c in s:
+        if c.isdigit():
+            print("True")
+            break
+    else:
+        print("False")
+
+    # Check if any character is lowercase
+    for c in s:
+        if c.islower():
+            print("True")
+            break
+    else:
+        print("False")
+
+    # Check if any character is uppercase
+    for c in s:
+        if c.isupper():
+            print("True")
+            break
+    else:
+        print("False")
