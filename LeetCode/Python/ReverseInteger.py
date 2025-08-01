c2327a1862100af7abdf671feacb633e918c7dbd
jarrodrein <jarrod1rein@gmail.com>
2025-08-01 15:05:30 -0500

    for base 10 negatives

--- LeetCode/Python/ReverseInteger.py
+++ LeetCode/Python/ReverseInteger.py
@@ -1,2 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        s = str(x)
+        if s[0] == '-':
+            s1 = '-' + s[:0:-1]
+        else:
+            s1 = s[::-1]
+        return int(s1)
