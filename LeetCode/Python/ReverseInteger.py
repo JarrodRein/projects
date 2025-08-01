73b7201cf2b632a17e0234040fadd6d176f04ee5
jarrodrein <jarrod1rein@gmail.com>
2025-08-01 15:14:38 -0500

    better check for integer overflow

--- LeetCode/Python/ReverseInteger.py
+++ LeetCode/Python/ReverseInteger.py
@@ -5,4 +5,9 @@ class Solution:
             s1 = '-' + s[:0:-1]
         else:
             s1 = s[::-1]
-        return int(s1)
+        result = int(s1)
+
+        # Check 32-bit signed integer range
+        if result < -2**31 or result > 2**31 - 1:
+            return 0
+        return result
