ca424681219da6731feffbd506f26da96cccff7a
jarrodrein <jarrod1rein@gmail.com>
2025-08-04 10:25:47 -0500

    if case for integer overflow.

--- LeetCode/Python/StringToInteger(Atoi).py
+++ LeetCode/Python/StringToInteger(Atoi).py
@@ -24,3 +24,8 @@ class Solution:
         INT_MIN = -2**31
         INT_MAX = 2**31 - 1
 
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+        return result
