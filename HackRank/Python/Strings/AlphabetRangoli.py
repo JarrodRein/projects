5b32c3427fbfe745d31e7c00bffbef95c1f75098
jarrodrein <jarrod1rein@gmail.com>
2025-08-05 17:09:32 -0500

    list created

--- HackRank/Python/Strings/AlphabetRangoli.py
+++ HackRank/Python/Strings/AlphabetRangoli.py
@@ -1,5 +1,9 @@
+import string
+
 def print_rangoli(size):
     # your code goes here
+    alphabet = list(string.ascii_lowercase)
+    print(alphabet)
 
 if __name__ == '__main__':
     n = int(input())
