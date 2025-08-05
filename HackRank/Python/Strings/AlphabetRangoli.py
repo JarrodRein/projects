361f0d85d0e2b9b1e75fb33556af577a789a8150
jarrodrein <jarrod1rein@gmail.com>
2025-08-05 17:15:22 -0500

    top half crafting

--- HackRank/Python/Strings/AlphabetRangoli.py
+++ HackRank/Python/Strings/AlphabetRangoli.py
@@ -4,6 +4,13 @@ def print_rangoli(size):
     # your code goes here
     alphabet = list(string.ascii_lowercase)
     print(alphabet)
+    
+    #top half
+    for i in range (0, size, 2):
+        print((alphabet[i + size]*i).center(size,"-"))
+    
+    print(alphabet[0].center(size, "-"))
+        
 
 if __name__ == '__main__':
     n = int(input())
