3cf17daf4ed5a3a4ec97324c2e61cc456aa39471
jarrodrein <jarrod1rein@gmail.com>
2025-08-05 17:26:02 -0500

    top half working

--- HackRank/Python/Strings/AlphabetRangoli.py
+++ HackRank/Python/Strings/AlphabetRangoli.py
@@ -4,10 +4,14 @@ def print_rangoli(size):
     # your code goes here
     alphabet = list(string.ascii_lowercase)
     #print(alphabet)
-    
-    #top half
-    for i in range (size-1, 0, -1):
-        print((alphabet[i]).center((size -1) *(size-1),"-"))
+    width = 4 * size - 3  # total width of each line
+
+    # top half
+    for i in range(size - 1, 0, -1):
+        left = alphabet[size-1:i:-1]
+        center = alphabet[i:size]
+        line = '-'.join(left + center)
+        print(line.center(width, '-'))
     
     print(alphabet[0].center((size -1) *(size-1),"-"))
         
