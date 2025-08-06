78e277d24092da0a7dfa88a6e4ffb8c535f8aa43
jarrodrein <jarrod1rein@gmail.com>
2025-08-06 08:21:44 -0500

    better edge cases

--- HackRank/Python/Strings/Capitalize!.py
+++ HackRank/Python/Strings/Capitalize!.py
@@ -8,17 +8,24 @@ import sys
 
 # Complete the solve function below.
 def solve(s):
-    # s3 = s.split(" ")
-    # s1 = s3[0]
-    # s2 = s3[1]
-    # #print(s1)
-    # #print(s2)
-    # s11 = s1[0].capitalize()
-    # s22 = s2[0].capitalize()
-    # s11 += s1[1:len(s1)]
-    # s22 += s2[1:len(s2)]
-    # result = s11 + " " + s22
-   
+    s3 = s.split(" ")
+        # s1 = s3[0]
+        # s2 = s3[1]
+        # #print(s1)
+        # #print(s2)
+        # s11 = s1[0].capitalize()
+        # s22 = s2[0].capitalize()
+        # s11 += s1[1:len(s1)]
+        # s22 += s2[1:len(s2)]
+        # result = s11 + " " + s22
+    cap_words = []
+        
+    for word in s3:
+        if word and word[0].isalpha():
+            word = word[0].upper() + word[1:]
+        cap_words.append(word)
+    
+    result = " ".join(cap_words)
     return result
 
 if __name__ == '__main__':
