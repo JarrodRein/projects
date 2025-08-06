a20556672924d4c728a5f69d4c26c44229f2cc85
jarrodrein <jarrod1rein@gmail.com>
2025-08-06 08:54:16 -0500

    final commit. Slow though

--- LeetCode/Python/RegularExpressionMatching.py
+++ LeetCode/Python/RegularExpressionMatching.py
@@ -1,10 +1,14 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         #if string is bigger than pattern. Then return false. Has to be 1 - 1
-        if len(s) > len(p):
-            return False
-        elif len(s) < len(p):
-            return False
+        if not p:
+            return not s
+        first_match = bool(s) and (p[0] == s[0] or p[0] == '.')
+
+        if len(p) >=2 and p[1] =='*':
+            # Try zero occurrence of p[0] (i.e., skip 2 in pattern)
+            # Or, if match, move string forward
+            return (self.isMatch(s, p[2:]) or (first_match and self.isMatch(s[1:], p)))
         else:
-            return True
+            return first_match and self.isMatch(s[1:], p[1:])
         
