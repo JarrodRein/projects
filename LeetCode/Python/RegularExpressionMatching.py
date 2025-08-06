b6e4c82a230a8e246446160799098d9043e55ea8
jarrodrein <jarrod1rein@gmail.com>
2025-08-06 08:32:23 -0500

    comment

--- LeetCode/Python/RegularExpressionMatching.py
+++ LeetCode/Python/RegularExpressionMatching.py
@@ -1,4 +1,7 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        #if string is bigger than pattern. Then return false. Has to be 1 - 1
         if len(s) >= len(p):
             return False
+
+        
