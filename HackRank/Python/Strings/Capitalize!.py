353a4a2da406a8aa4c5c6093c4bf0f0ebedac39a
jarrodrein <jarrod1rein@gmail.com>
2025-08-06 08:05:05 -0500

    split the string

--- HackRank/Python/Strings/Capitalize!.py
+++ HackRank/Python/Strings/Capitalize!.py
@@ -8,6 +8,12 @@ import sys
 
 # Complete the solve function below.
 def solve(s):
+    s3 = s.split(" ")
+    s1 = s3[0]
+    s2 = s3[1]
+    print(s1)
+    print(s2)
+    return s1 + s2
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
