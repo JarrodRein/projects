0c7ad80257a677ebb002c54a9ec964993f2fac2f
jarrodrein <jarrod1rein@gmail.com>
2025-08-08 09:51:25 -0500

    list of vowels added

--- HackRank/Python/Strings/TheMinionGame.py
+++ HackRank/Python/Strings/TheMinionGame.py
@@ -1,5 +1,8 @@
 def minion_game(string):
     # your code goes here
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in range(0, len(string), 1):
+        
 
 if __name__ == '__main__':
     s = input()
