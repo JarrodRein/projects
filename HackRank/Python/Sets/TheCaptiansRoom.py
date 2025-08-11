c5a2e41ac1ad3252a1b458f03ff5808786546d37
jarrodrein <jarrod1rein@gmail.com>
2025-08-11 10:23:02 -0500

    final commit

--- HackRank/Python/Sets/TheCaptiansRoom.py
+++ HackRank/Python/Sets/TheCaptiansRoom.py
@@ -4,3 +4,7 @@ n = int(input())
 m = list(map(int, input().split()))
 #Create a set to store unique elements
 #print(m)
+
+captian = (n * sum(set(m)) - sum(m)) // (n - 1)
+print(captian)
+#captian = (n * sum(set(m)) - sum(m)) / (n -
