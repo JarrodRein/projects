2a7d62c66422fcf23a9b068684ff93fad1ae0696
jarrodrein <jarrod1rein@gmail.com>
2025-08-12 16:25:19 -0500

    comments

--- LeetCode/Python/RomanToInteger.py
+++ LeetCode/Python/RomanToInteger.py
@@ -9,15 +9,20 @@ class Solution:
             'D': 500,
             'M': 1000
         }
-
+        #initialize total and previous value
         total = 0
         prev_value = 0
+        #loop through the string in reverse order
         for char in reversed(s):
+            #get the value from the map
             value = val_map[char]
+            #if the value is less than the previous value, subtract it
+            #otherwise, add it
             if value < prev_value:
                 total -= value
             else:
                 total += value
+            #update the previous value
             prev_value = value
-
+        
         return total
