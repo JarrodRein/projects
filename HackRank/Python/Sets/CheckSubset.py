6cc1c278924ed7a5eca382d1c163466fcadb027c
jarrodrein <jarrod1rein@gmail.com>
2025-08-14 15:41:45 -0500

    first attempt but not all test cases

--- HackRank/Python/Sets/CheckSubset.py
+++ HackRank/Python/Sets/CheckSubset.py
@@ -1,1 +1,13 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+m = list(map(int, input().split()))
+m = set(m)
+n = int(input())
+result = 'True' 
+for i in range(n):
+    a = set(map(int, input().split()))
+    result = m.issubset(a)
+print(result)
+#Create a set to store unique elements
+#print(m)
