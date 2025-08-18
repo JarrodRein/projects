d505eaa65b0f6de5088056d8161b54777981dd9b
jarrodrein <jarrod1rein@gmail.com>
2025-08-18 09:46:51 -0500

    final commit

--- HackRank/Python/Sets/SetDiscardRemovePop.py
+++ HackRank/Python/Sets/SetDiscardRemovePop.py
@@ -3,5 +3,13 @@ s = set(map(int, input().split()))
 m = int(input())
 
 for i in range (0, m):
-    s1 = input()
+    s1 = input().strip().split()
+    if s1[0] == "pop":
+        s.pop()
+    elif s1[0] == "remove":
+        s.remove(int(s1[1]))
+    elif s1[0] == "discard":
+        s.discard(int(s1[1]))
+#print(s)
+print(sum(s))
     #print(s1)
