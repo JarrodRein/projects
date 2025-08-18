d2a50f38f0a24a3d49a9e5c1dd09e6637dcc13dd
jarrodrein <jarrod1rein@gmail.com>
2025-08-18 11:39:14 -0500

    comments

--- LeetCode/Python/3Sum.py
+++ LeetCode/Python/3Sum.py
@@ -8,11 +8,13 @@ class Solution:
             # First element cannot repeat
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
+            # Two pointers for the other two elements
             target = -nums[i]
-            l, r = i + 1, n - 1
+            l, r = i + 1, n - 1 # Two pointers start after the first element and at the end of the list
+            # Move pointers towards each other
 
-            while l < r:
+            while l < r: # Ensure pointers do not cross
+                # Calculate the sum of the three elements
                 s = nums[l] + nums[r]
                 if s == target:
                     res.append([nums[i], nums[l], nums[r]])
@@ -25,9 +27,10 @@ class Solution:
                     while l < r and nums[r] == nums[r + 1]:
                         r -= 1
 
-                elif s < target:
+                elif s < target: # If the sum is less than the target, move the left pointer to increase the sum
                     l += 1
                 else:
                     r -= 1
-
+            # End of while loop, move to the next first element
+        # Return the list of triplets found
         return res
