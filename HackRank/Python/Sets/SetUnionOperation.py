7004913f19b0089d0f65015e8af254dd5c9c715b
jarrodrein <jarrod1rein@gmail.com>
2025-08-18 11:19:24 -0500

    english

--- HackRank/Python/Sets/SetUnionOperation.py
+++ HackRank/Python/Sets/SetUnionOperation.py
@@ -1,1 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# read English subscribers
+n = int(input())
+E = set(map(int, input().split()))
+
