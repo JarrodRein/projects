92dc12d56808ed894824a8662fc0ee98e3657b5f
jarrodrein <jarrod1rein@gmail.com>
2025-08-19 09:48:30 -0500

    set number correct. just need to count

--- HackRank/Python/Sets/SetIntersectionOperation.py
+++ HackRank/Python/Sets/SetIntersectionOperation.py
@@ -1,1 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+s = set(map(int, input().split()))
+m = int(input())
+s2 = set(map(int, input().split()))
+
+# Calculate the intersection of two sets
+
--- HackRank/Python/Sets/SetUnionOperation.py
+++ HackRank/Python/Sets/SetUnionOperation.py
@@ -1,15 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-# read English subscribers
 n = int(input())
-E = set(map(int, input().split()))
+s = set(map(int, input().split()))
+m = int(input())
+s2 = set(map(int, input().split()))
 
-# read French subscribers
-b = int(input())
-F = set(map(int, input().split()))
-
-# number of students with at least one subscription = |E F|
-print(len(E | F)) 
-
-# EOF
+# Calculate the intersection of two sets
+print((s.intersection(s2)))
