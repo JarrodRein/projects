0c7f22e16337e7cd2d0ee37c26749690bce0e521
jarrodrein <jarrod1rein@gmail.com>
2025-08-20 10:32:53 -0500

    value map added

--- LeetCode/Python/LetterCombinationsOfAPhoneNumber.py
+++ LeetCode/Python/LetterCombinationsOfAPhoneNumber.py
@@ -1,3 +1,13 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
+        # Telephone keypad value map
+        keypad_map = {
+            "2": ["A", "B", "C"],
+            "3": ["D", "E", "F"],
+            "4": ["G", "H", "I"],
+            "5": ["J", "K", "L"],
+            "6": ["M", "N", "O"],
+            "7": ["P", "Q", "R", "S"],
+            "8": ["T", "U", "V"],
+            "9": ["W", "X", "Y", "Z"]
+        }
