5ec28525d98b494ce30e42e565cea33222622ef1
jarrodrein <jarrod1rein@gmail.com>
2025-08-20 10:44:05 -0500

    better case

--- LeetCode/Python/LetterCombinationsOfAPhoneNumber.py
+++ LeetCode/Python/LetterCombinationsOfAPhoneNumber.py
@@ -1,18 +1,16 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # Telephone keypad value map
         keypad_map = {
-            "2": ["A", "B", "C"],
-            "3": ["D", "E", "F"],
-            "4": ["G", "H", "I"],
-            "5": ["J", "K", "L"],
-            "6": ["M", "N", "O"],
-            "7": ["P", "Q", "R", "S"],
-            "8": ["T", "U", "V"],
-            "9": ["W", "X", "Y", "Z"]
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
         }
         if not digits:
             return []
         elif len(digits) == 1:
             return keypad_map[digits]
-        else:
