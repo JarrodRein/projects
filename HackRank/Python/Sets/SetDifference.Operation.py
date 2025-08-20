c71037e761b81b668b84af47cf7877c3b4023425
jarrodrein <jarrod1rein@gmail.com>
2025-08-20 10:20:20 -0500

    set not list

--- HackRank/Python/Sets/SetDifference.Operation.py
+++ HackRank/Python/Sets/SetDifference.Operation.py
@@ -1,8 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n = int(input())
-m = list(map(int, input().split()))
+m = set(map(int, input().split()))
 
 n1 = int(input())
-m1 = list(map(int, input().split()))
+m1 = set(map(int, input().split()))
 
 print(len(m.difference(m1)))
