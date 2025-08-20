e6208311de9c7cab5290f22aa79da09c858f962a
jarrodrein <jarrod1rein@gmail.com>
2025-08-20 10:18:21 -0500

    second half captured

--- HackRank/Python/Sets/SetDifference.Operation.py
+++ HackRank/Python/Sets/SetDifference.Operation.py
@@ -1,3 +1,6 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n = int(input())
 m = list(map(int, input().split()))
+
+n1 = int(input())
+m1 = list(map(int, input().split()))
