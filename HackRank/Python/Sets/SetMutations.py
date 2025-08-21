c04f2b28629e3900f91ea6cba90b66ac0acf6e4b
jarrodrein <jarrod1rein@gmail.com>
2025-08-21 10:19:12 -0500

    logic made with help

--- HackRank/Python/Sets/SetMutations.py
+++ HackRank/Python/Sets/SetMutations.py
@@ -3,3 +3,17 @@ n = int(input())
 m = set(int, input().split())
 n1 = int(input())
 
+for _ in range(n1):
+    command = input().split()
+    operation = command[0]
+    num_elements = int(command[1])
+    elements = set(map(int, input().split()))
+    
+    if operation == "update":
+        m.update(elements)
+    elif operation == "intersection_update":
+        m.intersection_update(elements)
+    elif operation == "difference_update":
+        m.difference_update(elements)
+    elif operation == "symmetric_difference_update":
+        m.symmetric_difference_update(elements)
