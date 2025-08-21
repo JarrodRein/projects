1f0cc0b4c7fc8ac3c23ab605674b0cfe09544782
jarrodrein <jarrod1rein@gmail.com>
2025-08-21 10:29:03 -0500

    1st while loop

--- LeetCode/Python/4Sum.py
+++ LeetCode/Python/4Sum.py
@@ -18,4 +18,9 @@ class Solution:
                 # Move pointers towards each other
                 while l < r: # Ensure pointers do not cross
                     s = nums[l] + nums[r] # Calculate the sum of the two elements
-                   
+                    if s == target_2: # If the sum matches the target
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        # Move both pointers, skipping duplicates
+                        l += 1
+                        r -= 1
+                        
