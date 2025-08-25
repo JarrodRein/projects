023c0838cfd15294337e7eccdf85a0ef1e2aeca8
jarrodrein <jarrod1rein@gmail.com>
2025-08-25 10:25:56 -0500

    final commit

--- HackRank/Python/Built-Ins/Zipped.py
+++ HackRank/Python/Built-Ins/Zipped.py
@@ -1,1 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+N, X = map(int, input().split())
+
+
+mark = []
+for i in range (X):
+    mark.append(list(map(float, input().split())))
+
+for i in zip(*mark):
+    print("{:.1f}".format(sum(i)/len(i)))
