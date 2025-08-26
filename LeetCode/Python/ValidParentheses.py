a3171908080a64c34b802280036d1e02108fa77a
jarrodrein <jarrod1rein@gmail.com>
2025-08-26 12:36:51 -0500

    default cases added

--- LeetCode/Python/ValidParentheses.py
+++ LeetCode/Python/ValidParentheses.py
@@ -1,3 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        n = len(s)
+
+        if n % 2 != 0:
+            return False
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
         
