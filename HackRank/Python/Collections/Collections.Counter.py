939e20c95534659582184900175a9a3515c9d4b1
jarrodrein <jarrod1rein@gmail.com>
2025-09-02 08:59:32 -0500

    input captured

--- HackRank/Python/Collections/Collections.Counter.py
+++ HackRank/Python/Collections/Collections.Counter.py
@@ -1,1 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+integer_list = map(int, input().split())
+m = int(input())
+for _ in range(m):
+    a, b = map(int, input().split())
+    
