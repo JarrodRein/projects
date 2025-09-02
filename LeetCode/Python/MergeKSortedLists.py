fd2d2ce3d2bcdb20fc0b35d1dce87c9212a141b9
jarrodrein <jarrod1rein@gmail.com>
2025-09-02 09:13:25 -0500

    logic added but function def needed

--- LeetCode/Python/MergeKSortedLists.py
+++ LeetCode/Python/MergeKSortedLists.py
@@ -10,4 +10,11 @@ class Solution:
             return None
         if k == 1:
             return lists[0] # Only one list, return it as is.
+        if k == 2:
+            return self.mergeTwoLists(lists[0], lists[1])   # Use the helper function to merge two lists.   
         
+        mid = k // 2
+        left_merged = self.mergeKLists(lists[:mid])  # Recursively merge the left half.
+        right_merged = self.mergeKLists(lists[mid:]) # Recursively merge the right half.  
+        return self.mergeTwoLists(left_merged, right_merged)
+    
