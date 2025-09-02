96b066cfc98f7d0d4aff5d093007eddae2a39aca
jarrodrein <jarrod1rein@gmail.com>
2025-09-02 09:11:13 -0500

    edgecases added

--- LeetCode/Python/MergeKSortedLists.py
+++ LeetCode/Python/MergeKSortedLists.py
@@ -5,4 +5,9 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        k = len(lists)
+        if k == 0:
+            return None
+        if k == 1:
+            return lists[0] # Only one list, return it as is.
         
