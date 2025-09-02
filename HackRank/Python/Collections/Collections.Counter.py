b46161fdc2c4e80d3fdbc56db031a7d54029b951
jarrodrein <jarrod1rein@gmail.com>
2025-09-02 09:05:20 -0500

    counter and total added

--- HackRank/Python/Collections/Collections.Counter.py
+++ HackRank/Python/Collections/Collections.Counter.py
@@ -2,7 +2,10 @@ from collections import Counter
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n = int(input())
 integer_list = map(int, input().split())
+items = Counter(integer_list)
 m = int(input())
+total = 0
 for _ in range(m):
     a, b = map(int, input().split())
+
     
