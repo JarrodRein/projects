729502d1f03edb8f47fbbd49e29b61ba6cf194c3
jarrodrein <jarrod1rein@gmail.com>
2025-09-02 09:13:42 -0500

    function/method added

--- LeetCode/Python/MergeKSortedLists.py
+++ LeetCode/Python/MergeKSortedLists.py
@@ -18,3 +18,17 @@ class Solution:
         right_merged = self.mergeKLists(lists[mid:]) # Recursively merge the right half.  
         return self.mergeTwoLists(left_merged, right_merged)
     
+    
+    def mergeTwoLists(self, l1: Optional['ListNode'], l2: Optional['ListNode']) -> Optional['ListNode']:
+        dummy = ListNode(0)
+        tail = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        tail.next = l1 or l2
+        return dummy.next
