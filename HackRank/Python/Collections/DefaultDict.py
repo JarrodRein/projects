4ea69354ab0a87e2bff783836822fe526928544e
jarrodrein <jarrod1rein@gmail.com>
2025-09-03 17:53:38 -0500

    commented

--- HackRank/Python/Collections/DefaultDict.py
+++ HackRank/Python/Collections/DefaultDict.py
@@ -1,13 +1,15 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from collections import defaultdict
-d = defaultdict(list)
-n, m = map(int, input().split())
-for i in range(1, n+1):
-    word = input().strip()
-    d[word].append(i)
-for j in range(m):
-    query = input().strip()
-    if query in d:
-        print(' '.join(map(str, d[query])))
-    else:
+d = defaultdict(list) # default value is an empty list
+
+n, m = map(int, input().split()) # n = number of words, m = number of queries
+
+for i in range(1, n+1): # i is the line number, starting from 1, n is inclusive
+    word = input().strip() # read the word
+    d[word].append(i) # append the line number to the list of the word
+for j in range(m): # for each query
+    query = input().strip() # read the query
+    if query in d: # if the query is in the dictionary
+        print(' '.join(map(str, d[query]))) # print the line numbers as space-separated values
+    else: # if the query is not in the dictionary
         print(-1)
