9527b660de821958d44d7563d43123276401a717
jarrodrein <jarrod1rein@gmail.com>
2025-09-03 17:51:35 -0500

    final commit

--- HackRank/Python/Collections/DefaultDict.py
+++ HackRank/Python/Collections/DefaultDict.py
@@ -1,1 +1,13 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+d = defaultdict(list)
+n, m = map(int, input().split())
+for i in range(1, n+1):
+    word = input().strip()
+    d[word].append(i)
+for j in range(m):
+    query = input().strip()
+    if query in d:
+        print(' '.join(map(str, d[query])))
+    else:
+        print(-1)
