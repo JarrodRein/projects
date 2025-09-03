abfb7ffd383daea1e07a0649091a30c20d53b0b9
jarrodrein <jarrod1rein@gmail.com>
2025-09-03 15:54:17 -0500

    final commit

--- LeetCode/Python/SwapNodesInPairs.py
+++ LeetCode/Python/SwapNodesInPairs.py
@@ -5,4 +5,22 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        n = head
+        if not n or not n.next:
+            return head
+        newHead = head.next
+        prev = None
+        while n and n.next:
+            first = n
+            second = n.next
+            nxt = n.next.next
+            
+            if prev:
+                prev.next = second
+            second.next = first
+            first.next = nxt
+            
+            prev = first
+            n = nxt
+
+        return newHead
