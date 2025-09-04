f1197d834b28823546ffd0452f1d6eb59b479377
jarrodrein <jarrod1rein@gmail.com>
2025-09-04 10:27:45 -0500

    final commit

--- LeetCode/Python/FindClosestPerson.py
+++ LeetCode/Python/FindClosestPerson.py
@@ -1,3 +1,10 @@
 class Solution:
     def findClosest(self, x: int, y: int, z: int) -> int:
-    
+        dif = z - y 
+        dif2 = z -x
+        if abs(dif) < abs(dif2):
+            return 2
+        if abs(dif) > abs(dif2):
+            return 1
+        else:
+            return 0
