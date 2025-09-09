f1a6f099f90b4c23fc0d83db1808600eefb328b6
jarrodrein <jarrod1rein@gmail.com>
2025-09-09 12:33:45 -0500

    final commit

--- HackRank/Python/Collections/OrdersDict.py
+++ HackRank/Python/Collections/OrdersDict.py
@@ -1,1 +1,16 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import OrderedDict
+
+n = int(input())
+orders = OrderedDict()
+for _ in range(n):
+    item, price = input().rsplit(' ', 1)
+    price = int(price)
+    if item in orders:
+        orders[item] += price
+    else:
+        orders[item] = price
+
+for item, total_price in orders.items():
+        print(item, total_price)
+        
