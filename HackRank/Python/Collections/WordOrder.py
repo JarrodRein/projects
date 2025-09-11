b27c49c407f3516b13b29aa8c2c42c1b8dcb4717
jarrodrein <jarrod1rein@gmail.com>
2025-09-11 12:12:56 -0500

    final commit 3.4 python

--- HackRank/Python/Collections/WordOrder.py
+++ HackRank/Python/Collections/WordOrder.py
@@ -1,1 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import OrderedDict
+
+n = int(input())
+counts = OrderedDict()
+
+for _ in range(n):
+    w = input().strip()
+    counts[w] = counts.get(w, 0) + 1
+
+# print number of distinct words
+print(len(counts))
+# print counts in order of first appearance
+print(" ".join(str(c) for c in counts.values()))
