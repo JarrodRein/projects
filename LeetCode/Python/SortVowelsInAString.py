01780afec1b14004fa8e6f4df74b5d773666c6ae
jarrodrein <jarrod1rein@gmail.com>
2025-09-11 12:18:49 -0500

    inputs and vars

--- LeetCode/Python/SortVowelsInAString.py
+++ LeetCode/Python/SortVowelsInAString.py
@@ -1,3 +1,7 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        
+        vowels = "aeiouAEIOU"
+
+        extracted = sorted([c for c in s if c in vowels])
+        j = 0
+        result =[]
