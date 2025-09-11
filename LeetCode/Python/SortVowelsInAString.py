528c8f85b6e7249d2ad3019170cef1eaac0b641a
jarrodrein <jarrod1rein@gmail.com>
2025-09-11 12:19:54 -0500

    final commit

--- LeetCode/Python/SortVowelsInAString.py
+++ LeetCode/Python/SortVowelsInAString.py
@@ -5,3 +5,12 @@ class Solution:
         extracted = sorted([c for c in s if c in vowels])
         j = 0
         result =[]
+
+        for c in s:
+            if c in vowels:
+                result.append(extracted[j])
+                j += 1
+            else:
+                result.append(c)    
+        
+        return "".join(result)
