eb4a2ce98c6c2c25aa757d54dce9dd95c6b52db2
jarrodrein <jarrod1rein@gmail.com>
2025-09-16 08:34:27 -0500

    final commit

--- LeetCode/Python/ReverseNodesinKGroup.py
+++ LeetCode/Python/ReverseNodesinKGroup.py
@@ -6,3 +6,30 @@
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
+        def get_kth(curr: ListNode, k: int) -> ListNode | None:
+            while curr and k > 0:
+                curr = curr.next
+                k -= 1
+            return curr
+        
+        dummy = ListNode(0, head)
+        group_prev = dummy
+
+        while True:
+            kth = get_kth(groupPrev, k)
+            if npt kth:
+                break
+            groupNext = kth.next
+            # reverse group
+            prev, curr = kth.next, group_prev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+
+        return dummy.next
