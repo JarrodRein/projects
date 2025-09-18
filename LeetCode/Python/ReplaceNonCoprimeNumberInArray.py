2d6b2a17a77c1a38354cd9a0ecc44e6d390d1d26
jarrodrein <jarrod1rein@gmail.com>
2025-09-18 11:03:26 -0500

    final commit

--- LeetCode/Python/ReplaceNonCoprimeNumberInArray.py
+++ LeetCode/Python/ReplaceNonCoprimeNumberInArray.py
@@ -1,3 +1,14 @@
 class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
-        
+        st = []
+        for x in nums:
+            cur = x
+            # Keep merging with the stack top while non-coprime
+            while st:
+                g = gcd(st[-1], cur)
+                if g == 1:
+                    break
+                cur = (st[-1] // g) * cur   # LCM(st[-1], cur) but avoiding overflow
+                st.pop()
+            st.append(cur)
+        return st
