9200d98cc1b53aaf3c16df0e2dc0d1bb1e10379d
jarrodrein <jarrod1rein@gmail.com>
2025-09-18 10:19:24 -0500

    first attempt

--- HackRank/MYSQL/Aggregation/TheBlunder.sql
+++ HackRank/MYSQL/Aggregation/TheBlunder.sql
@@ -5,3 +5,11 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
+
+SELECT CEIL(
+         ABS(
+             AVG(SALARY) 
+           - AVG(CAST(REPLACE(SALARY,'0','') AS DECIMAL))
+         ) / 12
+       ) AS error
+FROM EMPLOYEES;
