2f69cd7b2a476303c3bcf89926984d7c5eca8c64
jarrodrein <jarrod1rein@gmail.com>
2025-09-19 08:55:47 -0500

    comments

--- HackRank/Python/Collections/CompanyLogo.py
+++ HackRank/Python/Collections/CompanyLogo.py
@@ -9,7 +9,7 @@ cnt = Counter(s)
 for ch, c in sorted(cnt.items(), key=lambda x: (-x[1], x[0]))[:3]:
     print(ch, c)
 
-    cnt.items() gives a list of pairs like [('b', 3), ('a', 2), ('c', 1), ('d', 1), ('e', 1)].
+    cnt.items() #gives a list of pairs like [('b', 3), ('a', 2), ('c', 1), ('d', 1), ('e', 1)].
 
 # We sort with a custom key:
 # -x[1] means sort by frequency descending (higher counts first). The minus sign reverses the normal ascending order.
