91f273ae8516311744aad2a56b6c727044fa0e88
jarrodrein <jarrod1rein@gmail.com>
2025-09-25 10:09:42 -0500

    final commit

--- LeetCode/Python/Triangle.py
+++ LeetCode/Python/Triangle.py
@@ -1,3 +1,9 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        
+        for row in range(len(triangle) - 2, -1, -1):   # start from 2nd-to-last row up to 0
+            for col in range(len(triangle[row])):     # each element in that row
+                triangle[row][col] += min(
+                    triangle[row + 1][col],           # left child
+                    triangle[row + 1][col + 1]        # right child
+                )
+        return triangle[0][0]
