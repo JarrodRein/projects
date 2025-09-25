c8f6f0f41384357b593606b9037aff1ece9cc106
jarrodrein <jarrod1rein@gmail.com>
2025-09-25 08:51:42 -0500

    final commit

--- HackRank/Python/RegexAndParsing/DetectFloatingPointNumber.py
+++ HackRank/Python/RegexAndParsing/DetectFloatingPointNumber.py
@@ -2,9 +2,12 @@
 import re
 
 n = int(input()) # Number of test cases
-pattern = r'^[+-]?(\d+\.\d*|\.\d+|\d+)$' # Pattern to match floating point numbers
+pattern = r'^[+-]?(\d*\.\d+|\d+\.\d+)$' # Pattern to match only floating point numbers
 for _ in range(n):
     s = input().strip()
+    if s == "0":
+        print(False)
+        continue
     if re.match(pattern, s):
         print(True)
     else:
