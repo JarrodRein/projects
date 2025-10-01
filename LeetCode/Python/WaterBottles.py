29a614ccff30fbacd6f35924a7b7dc0704cce3ef
jarrodrein <jarrod1rein@gmail.com>
2025-10-01 09:32:22 -0500

    comments

--- LeetCode/Python/WaterBottles.py
+++ LeetCode/Python/WaterBottles.py
@@ -2,8 +2,8 @@ class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
         n = numBottles
         empty = numBottles
-        while empty >= numExchange:
-            n += empty // numExchange
-            empty = empty // numExchange + empty % numExchange
+        while empty >= numExchange: # While we have enough empty bottles to exchange
+            n += empty // numExchange # Drink the new bottles obtained from exchange
+            empty = empty // numExchange + empty % numExchange  # Update empty bottles count
         return n
 # This code calculates the total number of water bottles one can drink given an initial number of bottles and an exchange rate for empty bottles.
