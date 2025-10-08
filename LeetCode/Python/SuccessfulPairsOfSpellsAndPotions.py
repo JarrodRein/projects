6c322a7da7933e0267d41f8dc977f654543e853a
jarrodrein <jarrod1rein@gmail.com>
2025-10-08 18:13:47 -0500

    final commit

--- LeetCode/Python/SuccessfulPairsOfSpellsAndPotions.py
+++ LeetCode/Python/SuccessfulPairsOfSpellsAndPotions.py
@@ -1,3 +1,14 @@
+from bisect import bisect_left
+    
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        
+        potions.sort() # O(n log n)
+        m = len(potions)
+        res = []
+        for spell in spells: # O(m log n)
+            min_potion = (success + spell -1) //spell # ceiling division
+            idx = bicect_left(potions, min_potion)
+            count = m - idx
+            res.append(count)
+        return res
+    
