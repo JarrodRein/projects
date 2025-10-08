301492749a0f82369a586639351584d46bc306f7
jarrodrein <jarrod1rein@gmail.com>
2025-10-08 17:26:20 -0500

    final commit

--- HackRank/MYSQL/Aggregation/WeatherObservationStation16.sql
+++ HackRank/MYSQL/Aggregation/WeatherObservationStation16.sql
@@ -5,3 +5,6 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
+SELECT CAST(ROUND(MIN(LAT_N),4) AS DECIMAL(10,4))
+FROM STATION
+WHERE LAT_N > 38.7780;
