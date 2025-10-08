576cf4487b654f3d97c7e7dcc76b621882d29139
jarrodrein <jarrod1rein@gmail.com>
2025-10-08 18:14:20 -0500

    comments

--- LeetCode/Python/SuccessfulPairsOfSpellsAndPotions.py
+++ LeetCode/Python/SuccessfulPairsOfSpellsAndPotions.py
@@ -7,8 +7,8 @@ class Solution:
         res = []
         for spell in spells: # O(m log n)
             min_potion = (success + spell -1) //spell # ceiling division
-            idx = bicect_left(potions, min_potion)
-            count = m - idx
-            res.append(count)
-        return res
+            idx = bicect_left(potions, min_potion) # O(log n)
+            count = m - idx # number of successful pairs
+            res.append(count)  # O(1)
+        return res # O(m log n + n log n) time, O(1) space
     
