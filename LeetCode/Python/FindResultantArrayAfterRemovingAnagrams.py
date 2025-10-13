84539e592c3198c6aee774eae6dafa76b9124403
jarrodrein <jarrod1rein@gmail.com>
2025-10-13 15:07:04 -0500

    final commit

--- LeetCode/Python/FindResultantArrayAfterRemovingAnagrams.py
+++ LeetCode/Python/FindResultantArrayAfterRemovingAnagrams.py
@@ -1,3 +1,10 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
+        res = [words[0]]  # start with the first word
         
+        for word in words[1:]:
+            # Compare sorted versions of last kept word and current
+            if sorted(word) != sorted(res[-1]):
+                res.append(word)
+                
+        return res
