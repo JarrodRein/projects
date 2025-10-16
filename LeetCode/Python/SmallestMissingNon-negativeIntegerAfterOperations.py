7ede42afc06b195eec8a4700872843109f2dceee
jarrodrein <jarrod1rein@gmail.com>
2025-10-16 12:14:31 -0500

    final commit

--- LeetCode/Python/SmallestMissingNon-negativeIntegerAfterOperations.py
+++ LeetCode/Python/SmallestMissingNon-negativeIntegerAfterOperations.py
@@ -1,3 +1,17 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
-        
+        n = len(nums)
+        mods = [0] * value
+        for num in nums:
+            mod = num % value
+            if mod < 0:
+                mod += value
+            mods[mod] += 1
+
+        ans = 0
+        while True:
+            mod = ans % value
+            if mods[mod] == 0:
+                return ans
+            mods[mod] -= 1
+            ans += 1
