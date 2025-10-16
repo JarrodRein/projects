35655e03698bb44aa529d846a07ee6b3540eeb9c
jarrodrein <jarrod1rein@gmail.com>
2025-10-16 16:23:02 -0500

    final commit

--- HackRank/Python/Math/TriangleQuest2.py
+++ HackRank/Python/Math/TriangleQuest2.py
@@ -1,7 +1,2 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-import math 
-
-n = int(input())
-
-for i in range(1, n+1):
-    print(((10**i - 1)//9)**2)
+for i in range(1, int(input())+1): print(((10**i - 1)//9)**2)
