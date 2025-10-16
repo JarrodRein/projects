760b43577d9ff3d5d4b60b677206c39c6694bc85
jarrodrein <jarrod1rein@gmail.com>
2025-10-15 22:06:26 -0500

    first attempt. Need degrees symbol

--- HackRank/Python/Math/FindAngleMBC.py
+++ HackRank/Python/Math/FindAngleMBC.py
@@ -1,1 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+n = int(input())
+m = int(input())
+
+angle_MBC = math.degrees(math.atan2(n, m))
+print(int(angle_MBC))   
