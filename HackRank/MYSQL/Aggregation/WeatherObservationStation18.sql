92e2af1c16f7258baad744d1e5980e842ab2299b
jarrodrein <jarrod1rein@gmail.com>
2025-10-15 22:01:56 -0500

    final commit

--- HackRank/MYSQL/Aggregation/WeatherObservationStation18.sql
+++ HackRank/MYSQL/Aggregation/WeatherObservationStation18.sql
@@ -5,5 +5,5 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
-SELECT ROUND(ABS(MAX(LAT_N) - MIN(LAT_N)) + ABS(MAX(LONG_W) - MIN(LONG_W)), 4)
+SELECT CAST(ROUND(ABS(MAX(LAT_N) - MIN(LAT_N)) + ABS(MAX(LONG_W) - MIN(LONG_W)), 4) AS DECIMAL(9,4)) AS result
 FROM STATION;
