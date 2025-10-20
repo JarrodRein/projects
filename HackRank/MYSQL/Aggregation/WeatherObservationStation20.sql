356accd9aa71dce7e53aa60c1c24f6e6741a427f
jarrodrein <jarrod1rein@gmail.com>
2025-10-20 11:41:15 -0500

    final commit

--- HackRank/MYSQL/Aggregation/WeatherObservationStation20.sql
+++ HackRank/MYSQL/Aggregation/WeatherObservationStation20.sql
@@ -5,3 +5,11 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
+SELECT TO_NUMBER(
+    CAST(
+        ROUND(
+            MIN(LAT_N) + MIN(LONG_W)
+        , 4
+        )
+    )   AS DECIMAL(9,4))
+FROM STATION;
