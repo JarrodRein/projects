cb2d5541d94dda3e7298a9e1a74914fde27bb12e
jarrodrein <jarrod1rein@gmail.com>
2025-10-20 11:29:03 -0500

    comments

--- LeetCode/Python/FinalvalueofVariableAfterPerformingOperations.py
+++ LeetCode/Python/FinalvalueofVariableAfterPerformingOperations.py
@@ -2,9 +2,9 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x = 0 # initial value
         for op in operations:
-            if op[1] == '+':
-                x += 1
-            else:
+            if op[1] == '+': # either "++X" or "X++" are in the same place
+                x += 1 # increment
+            else: # either "--X" or "X--" are in the same place and be deduced
                 x -= 1
         return x
     
