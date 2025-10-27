fda17af9d19741196b267af7f83756733f4c1c13
jarrodrein <jarrod1rein@gmail.com>
2025-10-27 13:44:33 -0500

    final commit

--- LeetCode/Python/NumberOfLaserBeamsInABank.py
+++ LeetCode/Python/NumberOfLaserBeamsInABank.py
@@ -1,3 +1,4 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        
+        banks = [row.count('1') for row in bank if '1' in row]
+        return sum(banks[i] * banks[i - 1] for i in range(1, len(banks)))   
