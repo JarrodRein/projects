74233e05bfe83c048aeea4c375971ef847f68004
jarrodrein <jarrod1rein@gmail.com>
2025-10-28 11:06:46 -0500

    second attempt and final commit

--- LeetCode/Python/MakeArrayElementsEqualToZero.py
+++ LeetCode/Python/MakeArrayElementsEqualToZero.py
@@ -1,24 +1,29 @@
-from typing import List
-
 class Solution:
     def countValidSelections(self, nums: List[int]) -> int:
-        curr = 0
-        if nums[curr] == 0:
-            return self.countValidSelections(nums[:curr] + nums[curr+1:])
-        elif nums[curr] > 0:
-            count = 0
-            for i in range(curr + 1, len(nums)):
-                if nums[i] < 0:
-                    nums[i] += 1
-                    count += self.countValidSelections(nums[:curr] + nums[curr+1:])
-                    nums[i] -= 1
-            return count
-        else:
-            count = 0
-            for i in range(curr + 1, len(nums)):
-                if nums[i] > 0:
-                    nums[i] -= 1
-                    count += self.countValidSelections(nums[:curr] + nums[curr+1:])
-                    nums[i] += 1
-            return count
-       
+        n = len(nums)
+        valid = 0
+        
+        # We can only start at indices where nums[i] == 0
+        for start in range(n):
+            if nums[start] != 0:
+                continue
+                
+            # Try both directions: -1 (left) and +1 (right)
+            for direction in [-1, 1]:
+                arr = nums[:]   # copy array since we'll mutate
+                curr = start
+                dir = direction
+                
+                while 0 <= curr < n:
+                    if arr[curr] == 0:
+                        curr += dir
+                    else:
+                        arr[curr] -= 1
+                        dir *= -1      # reverse direction
+                        curr += dir     # move in new direction
+                        
+                # If after walking off the array everything is zero, count it
+                if all(x == 0 for x in arr):
+                    valid += 1
+        
+        return valid
