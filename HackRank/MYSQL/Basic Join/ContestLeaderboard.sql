a3b1e231adac79669f722850f8039a0188912bdc
jarrodrein <jarrod1rein@gmail.com>
2025-10-29 18:36:19 -0500

    final commit

--- HackRank/MYSQL/Basic Join/ContestLeaderboard.sql
+++ HackRank/MYSQL/Basic Join/ContestLeaderboard.sql
@@ -6,7 +6,14 @@
     3. Type your code immediately after comment. Don't leave any blank line.
 */
 
-SELECT h.hacker_id, h.name 
+SELECT h.hacker_id, h.name, SUM(max_score) AS total_score
 FROM Hackers h
-JOIN Submissions s
- ON h.hacker_id = s.hacker_id;
+JOIN (
+    SELECT hacker_id, challenge_id, MAX(score) AS max_score
+    FROM Submissions
+    GROUP BY hacker_id, challenge_id
+) AS s
+  ON h.hacker_id = s.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING SUM(max_score) > 0
+ORDER BY total_score DESC, h.hacker_id ASC;
