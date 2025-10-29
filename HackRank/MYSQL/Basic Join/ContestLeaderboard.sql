9bca3d3811b42cf14b5ca68e9e82b9ad0b468c91
jarrodrein <jarrod1rein@gmail.com>
2025-10-29 18:35:37 -0500

    join working

--- HackRank/MYSQL/Basic Join/ContestLeaderboard.sql
+++ HackRank/MYSQL/Basic Join/ContestLeaderboard.sql
@@ -5,3 +5,8 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
+
+SELECT h.hacker_id, h.name 
+FROM Hackers h
+JOIN Submissions s
+ ON h.hacker_id = s.hacker_id;
