46dbf0ba7dd2d434b2653a6021e20076036a568b
jarrodrein <jarrod1rein@gmail.com>
2025-10-29 18:17:28 -0500

    comments

--- LeetCode/Python/SmallestNumberWithAllSetBits.py
+++ LeetCode/Python/SmallestNumberWithAllSetBits.py
@@ -1,7 +1,7 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        x = 1
-        while (1 << x ) - 1 < n:
-            x+=1
-        return (1 << x ) - 1
+        x = 1 # number of bits
+        while (1 << x ) - 1 < n: # all bits set
+            x+=1 # increment number of bits
+        return (1 << x ) - 1 # return number with all bits set
     
