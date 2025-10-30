0f66ad0c1ddd5af1119cf2cd2c494583d5b75bea
jarrodrein <jarrod1rein@gmail.com>
2025-10-30 18:18:12 -0500

    commented

--- LeetCode/Python/MinimunNumberofIncrementsonSubarraystoFormaTargetArray.py
+++ LeetCode/Python/MinimunNumberofIncrementsonSubarraystoFormaTargetArray.py
@@ -1,8 +1,8 @@
 class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
-        initial = target[0]
-        for i in range(1, len(target)):
-            if target[i] > target[i - 1]:
-                initial += target[i] - target[i - 1]
-        return initial
+        initial = target[0] # initial increments to reach first element
+        for i in range(1, len(target)):    # iterate through target
+            if target[i] > target[i - 1]: # if current is greater than previous
+                initial += target[i] - target[i - 1] # add difference to initial
+        return initial # return total increments needed
     
