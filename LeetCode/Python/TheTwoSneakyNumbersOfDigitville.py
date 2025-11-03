dcfb99f3e71c550435bf33aab068ddd249c3c39c
jarrodrein <jarrod1rein@gmail.com>
2025-11-03 10:27:28 -0600

    better big O

--- LeetCode/Python/TheTwoSneakyNumbersOfDigitville.py
+++ LeetCode/Python/TheTwoSneakyNumbersOfDigitville.py
@@ -8,3 +8,13 @@ class Solution:
                 if current == nums[j] and current not in results:
                     results.append(current)
         return results
+    
+    class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        res = []
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] == nums[j] and nums[i] not in res:
+                    res.append(nums[i])
+        return res
