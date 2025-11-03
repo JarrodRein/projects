9873f9b744deaeeced4203116b1f4306749a0fef
jarrodrein <jarrod1rein@gmail.com>
2025-11-03 10:27:08 -0600

    final commit, bruteforce

--- LeetCode/Python/TheTwoSneakyNumbersOfDigitville.py
+++ LeetCode/Python/TheTwoSneakyNumbersOfDigitville.py
@@ -1,3 +1,10 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        
+        results = []
+        n = len(nums)
+        for i in range(0, n-1):
+            current = nums[i]
+            for j in range(i+1, n):
+                if current == nums[j] and current not in results:
+                    results.append(current)
+        return results
