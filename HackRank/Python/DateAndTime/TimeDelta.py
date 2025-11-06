216b0f8aa182d294964ef6235a3de1831cc3b3c7
jarrodrein <jarrod1rein@gmail.com>
2025-11-06 07:46:46 -0600

    final commit

--- HackRank/Python/DateAndTime/TimeDelta.py
+++ HackRank/Python/DateAndTime/TimeDelta.py
@@ -5,9 +5,14 @@ import os
 import random
 import re
 import sys
+from datetime import datetime
 
 # Complete the time_delta function below.
 def time_delta(t1, t2):
+    fmt = "%a %d %b %Y %H:%M:%S %z"
+    dt1 = datetime.strptime(t1, fmt)
+    dt2 = datetime.strptime(t2, fmt)
+    return str(int(abs((dt1 - dt2).total_seconds())))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
