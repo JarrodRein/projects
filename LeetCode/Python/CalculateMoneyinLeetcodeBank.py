b0a7fbaf276fcac704840ca8a3dfb1c3a7d43561
jarrodrein <jarrod1rein@gmail.com>
2025-11-05 18:16:57 -0600

    first attempt

--- LeetCode/Python/CalculateMoneyinLeetcodeBank.py
+++ LeetCode/Python/CalculateMoneyinLeetcodeBank.py
@@ -1,3 +1,8 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        
+        total = 0
+        week = n // 7
+        day = n % 7
+        for i in range(n):
+            total += week + (i % 7) + 1
+        return total
