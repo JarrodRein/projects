d545088adbefbe977a91cf9f777da3b00faae30f
jarrodrein <jarrod1rein@gmail.com>
2025-11-06 07:28:04 -0600

    first attempt. No logic

--- LeetCode/Python/FindXSumofAllKLongSubarrays1.py
+++ LeetCode/Python/FindXSumofAllKLongSubarrays1.py
@@ -1,3 +1,12 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        
+        n = len(nums)
+        result = []
+        window_sum = sum(nums[:k])
+        if window_sum == x:
+            result.append(0)
+        for i in range(k, n):
+            window_sum += nums[i] - nums[i - k]
+            if window_sum == x:
+                result.append(i - k + 1)
+        return result
