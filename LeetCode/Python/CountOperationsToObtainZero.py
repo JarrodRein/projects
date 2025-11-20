45242eb37807b1c89bb668c6b7c7041e733c6bd9
jarrodrein <jarrod1rein@gmail.com>
2025-11-20 07:25:21 -0600

    better method and count implemented. final commit

--- LeetCode/Python/CountOperationsToObtainZero.py
+++ LeetCode/Python/CountOperationsToObtainZero.py
@@ -1,13 +1,10 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
         count = 0
-        while(num1>0&num2>0):
-            if num1>num2:
-                num1 = num1 - num2
-                count = count + 1
-            elif num2>num1:
-                num2 = num2 - num1
-                count = count + 1
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                num1 -= num2
             else:
-                continue
+                num2 -= num1
+            count += 1
         return count
