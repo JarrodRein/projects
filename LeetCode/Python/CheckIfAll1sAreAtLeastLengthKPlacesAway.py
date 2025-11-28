ea88d4233662cf46ecebde65e32c53145cac6322
jarrodrein <jarrod1rein@gmail.com>
2025-11-28 06:32:24 -0600

    final commit

--- LeetCode/Python/CheckIfAll1sAreAtLeastLengthKPlacesAway.py
+++ LeetCode/Python/CheckIfAll1sAreAtLeastLengthKPlacesAway.py
@@ -1,3 +1,11 @@
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
-        
+        count = 0
+        for num in nums:
+            if num == 1:
+                if count < k and count != 0:
+                    return False
+                count = 0
+            else:
+                count += 1
+        return True
