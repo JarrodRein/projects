3f6471e97af61f5b2ddf428499861d6b52eaea88
jarrodrein <jarrod1rein@gmail.com>
2025-11-28 06:48:20 -0600

    final commit

--- HackRank/Python/PythonFunctions/ReduceFunction.py
+++ HackRank/Python/PythonFunctions/ReduceFunction.py
@@ -2,7 +2,7 @@ from fractions import Fraction
 from functools import reduce
 
 def product(fracs):
-    t = # complete this line with a reduce statement
+    t = reduce(lambda x, y: x * y, fracs) # Multiply all fractions
     return t.numerator, t.denominator
 
 if __name__ == '__main__':
